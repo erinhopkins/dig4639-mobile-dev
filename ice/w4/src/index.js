cbb5a64f8b8ea41a78e410e4315e343c323443dd
Erin Hop <ehop@users.noreply.github.com>
2020-02-01 20:33:38 -0500

    lab 1 commit

--- ice/w4/src/index.js
+++ ice/w4/src/index.js
@@ -1,8 +1,8 @@
 import Component from "./Component.js";
-import Task from "./Task.js";
+import Task from "./task.js";
 function runOnLoad()
 {
-    
+
     let element = document.createElement("div");
     element.id = "Test";
     document.body.appendChild(element);
@@ -11,6 +11,11 @@ function runOnLoad()
     let element2 = document.createElement("div");
     element2.innerHTML = comp.render();
     console.log("Hello World truly");
+    let myTask = new Task({
+        content: "",
+        done: false
+    })
 }
+console.log(myTask.render());
 
 window.addEventListener("DOMContentLoaded", runOnLoad);
---
+++ ice/w4/src/task.js
@@ -0,0 +1,12 @@
+class Task {
+    constructor(props) {
+        this.props = props; 
+    }
+
+    render() {
+        console.log("beep");
+        return "<div></div>";
+    }
+}
+
+export default Task; 
