d99922fdfdb1240a1022bbc6595c1030b2b69ca5
Erin Hop <ehop@users.noreply.github.com>
2020-02-12 11:45:06 -0500

    Updates

---
+++ precache-manifest.cebbd54c2f1db9bc292456b2dbd284be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ccc6adf7c1d600e85495718a67f69c",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "9098df27e5c2a908a4da",
+    "url": "/dig4639-mobile-dev/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "e02cba94be16e740280c",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9098df27e5c2a908a4da",
+    "url": "/dig4639-mobile-dev/static/js/main.ddecd7ed.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
