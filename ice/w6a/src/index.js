73b3155efd1a5cee446029696469251393491c93
Erin Hop <ehop@users.noreply.github.com>
2020-02-12 12:11:50 -0500

    commit

--- ice/w6a/src/index.js
+++ ice/w6a/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  render() {
+  constructor(props) {
+		super(props);
+		this.state = {
+			value: null,
+		};
+	}
+	render() {
     return (
-      <button className="square" onClick={function() { alert('click'); }}>
-		{this.props.value}
+      <button className="square" onClick={() => { alert('click'); }}>
+				{this.props.value}
       </button>
     );
   }
