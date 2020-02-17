0bab74a95f279d20870d6476cd817bb5c802c9e7
Erin Hop <ehop@users.noreply.github.com>
2020-02-17 12:21:42 -0500

    ICE w7a AddList

--- ice/w7a/MyMath.js
+++ ice/w7a/MyMath.js
@@ -1,3 +1,35 @@
-export function Sum (a, b) {
-  return a + b
+function Sum (a, b) {
+	let result = undefined;
+	if(typeof a == "number" &&  typeof b == "number") {
+  	result = a + b
+	}
+	return result;
 }
+
+
+function AddList(arr) {
+	let result = undefined;
+	if(Array.isArray(arr) && arr.length > 0) {
+		result = 0;
+		for(var elem of arr) {
+			if(typeof elem != "number") {
+				result = undefined;
+				break;
+			}
+			result = result + elem;
+		}
+	}
+	return result;
+}
+
+export { Sum, AddList };
+
+
+
+
+//DivideBy
+// function divide(a, b) {
+//   return a / b;
+// }
+
+// export { divide };
--- ice/w7a/MyMath.test.js
+++ ice/w7a/MyMath.test.js
@@ -1,10 +1,54 @@
-import { Sum } from './sum.js'
+import { Sum, AddList } from './MyMath.js'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(Sum(1, 2)).toBe(3)
+// ICE
+describe("Sum", () => {
+
+	test('Whether undefined is returned on invalid', () => {
+		expect(Sum(1, "Test")).toBeUndefined()
+	})
+
+	test('adds 1 + 2 to equal 3', () => {
+		expect(Sum(1, 2)).toBe(3)
+	})
+
+	it('produces the sum of the sum of 10 + 20, which should be 30', () => {
+		expect(Sum(10, 20)).toBe(30)
+	})
 })
 
-// ICE 
+//AddList
+describe("AddList", () => {
+
+	test('Adds a list of numbers together', () => {
+		let numElem = 1000;
+		let input = [];
+		let result = 0;
+		var elem = 0;
+		for(var j = 0; j < 10; j++) {
+			for(var i = 0; i < numElem; i++) {
+				var elem = Math.random()*1000;
+				result += elem;
+				input.push(elem)
+			}
+					expect(AddList(input)).toBe(result);
+		}
+	})
+
+	test('Test if enmpty array returns undefined', () => {
+		let input = [];
+		expect(AddList(input)).toBeUndefined();
+	})
+
+	test('Test if enmpty array returns undefined', () => {
+		let input = [undefined,0,2,3];
+		expect(AddList(input)).toBeUndefined();
+	})
+	test('Test if non-array returns undefined', () => {
+		let input = 100;
+		expect(AddList(input)).toBeUndefined();
+	})
+})
+
 
 // Examples for Null
 test('null', () => {
