153c3d273ce6b727ca3fcd56d331c0b116d1e06a
Erin Hop <ehop@users.noreply.github.com>
2020-02-26 15:00:38 -0500

    lab 4 commit

---
+++ labs/4/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CardList from "./components/CardList/index.js";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CardList content="This is a card!"></CardList>
+    </div>
+  );
+}
+
+export default App;
