3265f73a002a35db82752585ae3e2f21169a0f52
Erin Hop <ehop@users.noreply.github.com>
2020-03-29 20:41:58 -0400

    lab 5 commit

---
+++ labs/5/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CardList from "./components/CardList";
+
+function App() {
+	return (
+		<div className="App">
+			<CardList></CardList>
+		</div>
+	);
+}
+
+export default App;
---
+++ labs/5/src/components/Card/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './index.css'
+
+class Card extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			name: '',
+			temperature: 0,
+			temperatureUnit: '',
+			detailedForecast: ''
+		}
+	}
+
+render() {
+  return (
+		<div className = "card">
+			<h2>{this.props.name}</h2>
+			<h1>{this.props.temperature} {this.props.temperatureUnit}</h1>
+			<p>{this.props.detailedForecast}</p>
+		</div>
+  	)
+	}
+}
+
+export default Card;
