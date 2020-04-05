607f682c13f502311219f6534056e5a78c817cf8
Erin Hop <ehop@users.noreply.github.com>
2020-04-04 21:09:39 -0400

    w12b

--- demo/w12b/App.js
+++ demo/w12b/App.js
@@ -1,27 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ListView } from 'react-native';
+import { getCurrentFrame } from 'expo/build/AR';
 
 export default class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.rows = ['This is 1', 'This is 2', 'This is 3']
+		const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+		this.state = {
+			content: "Hello world",
+			dataSource: ds.cloneWithRows(this.rows),
+			currentItem: 3
+	}
+		// console.log("Hello world!!!")
+	}
 
-  constructor(props) {
-    super(props)
-    this.state = {content: "Hello World"}
-    // console.log("Hello World!!!")
-  } 
+	onPressHandler(evt) {
+		console.log("Clicked!!")
+		this.setState({content: "Hello React Native blah"})
+		// this.rows.push('New data to be added')
+		// console.log("this.rows")
+		this.rows = [...this.rows, 'New Array Item']
+		this.setState({dataSource: this.state.dataSource.cloneWithRows(this.rows)})
+	}
 
-  onPressHandler(evt) {
-    console.log("Clicked!!")
-    this.setState({content: "Hello Glorious React Native World!"})
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.header}>{this.state.content}</Text>
-        <Button style={styles.button} color="#990000" title="Click Me" onPress={(evt) => this.onPressHandler(evt)}></Button>
-      </View>
-    );
-  }
+	render() {
+		return (
+				<View style={styles.container}>
+					<Text style={styles.header}>{this.state.content}</Text>
+					<Button style={styles.button} color="#90D9CB" title="Click Me" onPress={(evt) => this.onPressHandler(evt)}></Button>
+					 <ListView
+        		dataSource={this.state.dataSource}
+        		renderRow={(rowData) => <View><Button title="Complete"></ Button>
+						<Text key={rowData.id}>{rowData}</Text></View>}
+      />
+				</View>
+		);
+	}
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -30,10 +47,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header: {
-    fontSize: 22
-  },
-  button: {
+	button: {
 
-  }
+	}
 });
