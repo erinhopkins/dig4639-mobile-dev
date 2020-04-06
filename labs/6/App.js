81e82857b32701e0803d40814bad83752ea3e6e3
Erin Hop <ehop@users.noreply.github.com>
2020-04-06 17:06:02 -0400

    lab 6 w/o styling

--- labs/6/App.js
+++ labs/6/App.js
@@ -1,38 +1,136 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import Questions from 'components/questions.json'
+import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import questions from './questions.json'
 
-export default class App extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			loading: false,
-			questions: [],
-			current: 0,
-			correctScore: 5,
-			
-		}
-	}
-
-
-render() {
-	return (
-			<View style={styles.container}>
-				<Text>App 6</Text>
+// Quiz Question
+class QuizQuestion extends React.Component {
+	render() {
+		return (
+			<View>
+				<Text>{this.props.question + 1}</Text>
+				<Text>{ questions[this.props.question].question }</Text>
+				{ questions[this.props.question].possibleAnswers.map( ( answer, index ) => ( <Button title={answer.text} buttonStyle={styles.button} onPress={() => this.props.callback( answer.correct )} key={index}></Button> ) )}
 			</View>
-		);
+		)
 	}
 }
-  
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentQuestion: 0,
+			score: 0,
+			started: false,
+			previousQuestion: {
+				correct: null
+			},
+			showResult: false,
+			complete: false
+    }
+		this.nextQuestion = this.nextQuestion.bind(this);
+  }
+
+	// Start Quiz Method
+	startQuiz() {
+		this.setState({
+			started: true
+		});
+	}
+
+	// Move To Next Question
+	async nextQuestion( correct ) {
+		this.setState({
+			previousQuestion: {
+				correct
+			},
+			showResult: true
+		})
+
+		await new Promise( resolve => setTimeout( resolve, 1000) );
+
+		const currentQuestion = this.state.currentQuestion + 1;
+
+		this.setState({
+			score: correct ? this.state.score + 1 : this.state.score,
+			currentQuestion,
+			showResult: false,
+			complete: ( currentQuestion === questions.length ) ? true : false
+		});
 
 
+	}
 
+	// Show Results After Each Question
+	showQuestionResult() {
+		return this.state.previousQuestion.correct ?
+		// If Correct
+			(<View><Text>Correct!</Text></View>):
+
+		// If Wrong
+			(
+				<View><Text>Wrong!</Text></View>
+			)
+	}
+
+
+	// Final Quiz Resultsxdz
+	showQuizResults() {
+		return (
+			<View>
+				<Text>Congratulations</Text>
+				<Text>You Got {this.state.score} / {questions.length} Correct</Text>
+			</View>
+		)
+	}
+
+
+	// App Load Page
+	startPage() {
+		return (
+			<View containerStyle={styles.container}>
+				<Text style={styles.text}>Start the Quiz</Text>
+				<Button title="Start" style={styles.buttonStyle} onPress={() => this.startQuiz()}></Button>
+			</View>
+		)
+	}
+
+  render () {
+    if ( !this.state.started ) {
+			return this.startPage();
+		}
+
+		else if ( this.state.complete ) {
+			return this.showQuizResults();
+		}
+
+		else if ( this.state.showResult ) {
+			return this.showQuestionResult();
+		}
+
+		else {
+
+			return (
+				<QuizQuestion question={this.state.currentQuestion} callback={this.nextQuestion}></QuizQuestion>
+			)
+		}
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+		width: 400,
+		backgroundColor: "red"
   },
-});
+
+	text: {
+		textAlign: "center"
+	},
+
+	buttonStyles: {
+		color: "#90d9cb"
+	}
+
+
+})
